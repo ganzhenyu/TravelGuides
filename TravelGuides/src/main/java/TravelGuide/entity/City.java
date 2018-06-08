e63d2a9547d40a5717afb12492a27e1575a8d07f
KwanHenry <1484779144@qq.com>
2018-06-08 17:12:58 +0800

    test

--- TravelGuides/src/main/java/TravelGuide/entity/City.java
+++ TravelGuides/src/main/java/TravelGuide/entity/City.java
@@ -6,7 +6,6 @@ package TravelGuide.entity;
 public class City {
     private int id;
     private String name;
-    private String test;
 
     public City(int id, String name) {
         this.id = id;
