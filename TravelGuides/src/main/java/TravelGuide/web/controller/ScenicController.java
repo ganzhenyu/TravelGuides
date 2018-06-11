864f66c7d5b2b1bbb4a2a336b29cd43734d71773
KwanHenry <1484779144@qq.com>
2018-06-11 17:33:40 +0800

    6月11日更新

--- TravelGuides/src/main/java/TravelGuide/web/controller/ScenicController.java
+++ TravelGuides/src/main/java/TravelGuide/web/controller/ScenicController.java
@@ -1,11 +1,16 @@
 package TravelGuide.web.controller;
 
+import TravelGuide.entity.Scenic;
 import TravelGuide.mapper.ScenicMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Administrator on 2018/6/11.
@@ -18,11 +23,15 @@ public class ScenicController {
     private ScenicMapper scenicMapper;
 
     @GetMapping("/index")
-    public String index(Model model){
-        model.addAttribute("scenicByEndTime",scenicMapper.fetchByEndTime(3));
-        model.addAttribute("scenicByLove",scenicMapper.fetchByLove(3));
-        model.addAttribute("scenicByEvaluate",scenicMapper.fetchByEvaluate(3));
-        return "index";
+    public Map<String,Object> index(){
+        Map<String,Object> map = new HashMap<>();
+        List<Scenic> scenicByEndTime = scenicMapper.fetchByEndTime(3);
+        List<Scenic> scenicByLove = scenicMapper.fetchByEndTime(3);
+        List<Scenic> scenicByEvaluate = scenicMapper.fetchByEndTime(3);
+        map.put("scenicByEndTime",scenicByEndTime);
+        map.put("scenicByLove",scenicByLove);
+        map.put("scenicByEvaluate",scenicByEvaluate);
+        return map;
     }
 
 }
